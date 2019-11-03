7ae46d03473d40429f0609a3260edce1ad9b6533
JINGYANGJY <yang.jing4@northeastern.edu>
2019-11-03 16:10:46 -0500

    f7

--- webapp/src/main/java/com/java/JavaApplication.java
+++ webapp/src/main/java/com/java/JavaApplication.java
@@ -2,18 +2,11 @@ package com.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 @SpringBootApplication
-public class JavaApplication extends SpringBootServletInitializer {
+public class JavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(JavaApplication.class);
-    }
 }

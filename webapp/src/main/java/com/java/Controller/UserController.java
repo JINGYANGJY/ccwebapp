c48debbbfd2b506b54b2670508bc9e8dfb19e8bb
Shujie Fan <fan.shuj@northeastern.edu>
2019-11-05 17:53:18 -0500

    Added matrix support

--- webapp/src/main/java/com/java/Controller/UserController.java
+++ webapp/src/main/java/com/java/Controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Pattern;
+import static com.java.JavaApplication.statsDClient;
 
 @RestController
 public class UserController {
@@ -25,10 +26,14 @@ public class UserController {
     public @ResponseBody
     ResponseEntity<String>
     createAccount(@RequestBody ObjectNode objectNode) {
+        long startTime = System.currentTimeMillis();
+        statsDClient.incrementCounter("endpoint.user.http.post");
+
         if (objectNode.get("first_name") == null || objectNode.get("last_name") == null ||
                 objectNode.get("email_address") == null || objectNode.get("password") == null) {
             JSONObject jObject = new JSONObject();
             jObject.put("message", "Required info Cannot be null");
+            recordTime("endpoint.user.http.post", startTime);
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body(jObject.toString());
@@ -46,6 +51,7 @@ public class UserController {
         if (!isEmailMatch) {
             JSONObject jObject = new JSONObject();
             jObject.put("message", "Please use correct email");
+            recordTime("endpoint.user.http.post", startTime);
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body(jObject.toString());
@@ -54,13 +60,18 @@ public class UserController {
         if (!isPassMatch) {
             JSONObject jObject = new JSONObject();
             jObject.put("message", "Please use Strong Password");
+            recordTime("endpoint.user.http.post", startTime);
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body(jObject.toString());
         }
 
         String pw_hash = BCrypt.hashpw(password, BCrypt.gensalt());
+
+        long findQueryStart = System.currentTimeMillis();
         User user = userRepository.findUserByEmail(email);
+        recordTime("endpoint.user.http.post.query.findUserByEmail", findQueryStart);
+
         if (user == null) {
             Date dNow = new Date();
             SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
@@ -68,7 +79,11 @@ public class UserController {
             user.setAccountCreated(ft.format(dNow));
             user.setAccountUpdate(ft.format(dNow));
             user.setId(UUID.randomUUID().toString());
+
+            long saveQueryStart = System.currentTimeMillis();
             userRepository.save(user);
+            recordTime("endpoint.user.http.post.query.save", saveQueryStart);
+
             JSONObject jObject = new JSONObject();
             jObject.put("id", user.getId());
             jObject.put("first_name", user.getFirstName());
@@ -77,11 +92,14 @@ public class UserController {
             jObject.put("account_created", user.getAccountCreated());
             jObject.put("account_updated", user.getAccountUpdate());
 
+            recordTime("endpoint.user.http.post", startTime);
+
             return ResponseEntity.status(HttpStatus.OK).
                     body(jObject.toString());
         } else {
             JSONObject jObject = new JSONObject();
             jObject.put("message", "Email Address Already Exists");
+            recordTime("endpoint.user.http.post", startTime);
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body(jObject.toString());
@@ -92,13 +110,17 @@ public class UserController {
     @RequestMapping(value = "/v1/user/self", method = RequestMethod.GET)
     public @ResponseBody
     ResponseEntity<String> getUserInfo(@RequestHeader(value = "Authorization") String auth) {
+        long startTime = System.currentTimeMillis();
+        statsDClient.incrementCounter("endpoint.user.http.get");
+
         byte[] decodedBytes = Base64.getDecoder().decode(auth.split("Basic ")[1]);
         String decodedString = new String(decodedBytes);
         String email = decodedString.split(":")[0];
         String password = decodedString.split(":")[1];
-        User userAu = userRepository.findUserByEmail(email);
-        ;
 
+        long findQueryStart = System.currentTimeMillis();
+        User userAu = userRepository.findUserByEmail(email);
+        recordTime("endpoint.user.http.get.query.findUserByEmail", findQueryStart);
 
         if (Authentication(userAu, password)) {
             JSONObject jObject = new JSONObject();
@@ -109,11 +131,13 @@ public class UserController {
             jObject.put("account_created", userAu.getAccountCreated());
             jObject.put("account_updated", userAu.getAccountUpdate());
 
+            recordTime("endpoint.user.http.get", startTime);
             return ResponseEntity.status(HttpStatus.OK).
                     body(jObject.toString());
         } else {
             JSONObject jObject = new JSONObject();
             jObject.put("message", "Unable to get any other user info");
+            recordTime("endpoint.user.http.get", startTime);
             return ResponseEntity
                     .status(HttpStatus.UNAUTHORIZED)
                     .body(jObject.toString());
@@ -125,12 +149,17 @@ public class UserController {
     public @ResponseBody
     ResponseEntity<String>
     updateUserInfo(@RequestHeader(value = "Authorization") String auth, @RequestBody ObjectNode objectNode) {
+        long startTime = System.currentTimeMillis();
+        statsDClient.incrementCounter("endpoint.user.http.put");
+
         byte[] decodedBytes = Base64.getDecoder().decode(auth.split("Basic ")[1]);
         String decodedString = new String(decodedBytes);
         String email = decodedString.split(":")[0];
         String password = decodedString.split(":")[1];
+
+        long findQueryStart = System.currentTimeMillis();
         User userAu = userRepository.findUserByEmail(email);
-        ;
+        recordTime("endpoint.user.http.put.query.findUserByEmail", findQueryStart);
 
         if (Authentication(userAu, password)) {
             Iterator<String> fieldNames = objectNode.fieldNames();
@@ -139,6 +168,7 @@ public class UserController {
                 if (!(field.equals("first_name") || field.equals("last_name") || field.equals("password"))) {
                     JSONObject jObject = new JSONObject();
                     jObject.put("message", "Only first name, last name and password can be updated");
+                    recordTime("endpoint.user.http.put", startTime);
                     return ResponseEntity
                             .status(HttpStatus.BAD_REQUEST)
                             .body(jObject.toString());
@@ -159,6 +189,7 @@ public class UserController {
                 if (!isMatch) {
                     JSONObject jObject = new JSONObject();
                     jObject.put("message", "Please use Strong Password");
+                    recordTime("endpoint.user.http.put", startTime);
                     return ResponseEntity
                             .status(HttpStatus.BAD_REQUEST)
                             .body(jObject.toString());
@@ -169,14 +200,18 @@ public class UserController {
             userAu.setEmail(email);
             userAu.setFirstName(newFirstName);
             userAu.setLastName(newLastName);
+            long saveQueryStart = System.currentTimeMillis();
             userRepository.save(userAu);
+            recordTime("endpoint.user.http.put.query.save", saveQueryStart);
 
             JSONObject jObject = new JSONObject();
+            recordTime("endpoint.user.http.put", startTime);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).
                     body(jObject.toString());
         } else {
             JSONObject jObject = new JSONObject();
             jObject.put("message", "Unable to update any other user info");
+            recordTime("endpoint.user.http.put", startTime);
             return ResponseEntity
                     .status(HttpStatus.UNAUTHORIZED)
                     .body(jObject.toString());
@@ -208,5 +243,10 @@ public class UserController {
             return false;
         }
         return true;
+    }
+
+    public void recordTime(String name, Long startTime) {
+        long endTime = System.currentTimeMillis();
+        statsDClient.recordExecutionTime(name, endTime - startTime);
     }
 }
--- webapp/src/main/java/com/java/JavaApplication.java
+++ webapp/src/main/java/com/java/JavaApplication.java
@@ -1,9 +1,12 @@
 package com.java;
 
+import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.StatsDClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaApplication {
+    public static final StatsDClient statsDClient = new NonBlockingStatsDClient("", "localhost", 8125);
 
     public static void main(String[] args) {
         SpringApplication.run(JavaApplication.class, args);

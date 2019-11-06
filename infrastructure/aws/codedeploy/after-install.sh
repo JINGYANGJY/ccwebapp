742d33fde38ed42efd4e96fd0955b3289a28de2b
Shujie Fan <fan.shuj@northeastern.edu>
2019-11-06 02:25:28 -0500

    Test cloudwatch 5

--- infrastructure/aws/codedeploy/after-install.sh
+++ infrastructure/aws/codedeploy/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /etc/profile
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:~/cloudwatch-config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-config.json -s
 nohup java -jar ~/java-0.0.1-SNAPSHOT.jar >~/spring.log 2>&1 &
